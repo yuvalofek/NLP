0c8300a9c86d9ff88bbd0e4fa9a57b4ac69299e0
Yuval Ofek <35583988+yuvalofek@users.noreply.github.com>
2021-03-30 19:35:49 -0400

    unify parsing classes

--- Parser/main.py
+++ Parser/main.py
@@ -3,27 +3,37 @@ import re
 import logging
 
 
-class GrammarLoader:
-    def __init__(self, grammar_path):
+class Node:
+    def __init__(self, content):
+        self.right = None
+        self.left = None
+        self.content = content
+
+
+class Parser:
+    def __init__(self, grammar):
+        self.rules = None
+        self.load_grammar(grammar)
+
+    def load_grammar(self, grammar):
         self.rules = []
-        with open(grammar_path) as f:
+        with open(grammar) as f:
             logging.info('Grammar file opened')
             for line in f.readlines():
                 ln = line.split(' --> ')
                 right_arrow = re.sub('\n', '', ln[1])
                 self.rules.append((ln[0], right_arrow.split(' ')))
-
         logging.debug(self.rules)
         logging.info('Grammar rules created')
 
-
-class Parser:
-    def __init__(self, grammar):
-        self.grammar = grammar
-
     def parse(self, sentence):
-        sentence_list = sentence.split(' ')
-        for word in sentence_list:
+        sentence_ = sentence.split(' ')
+        n = len(sentence_)
+
+        # initialize?
+
+
+        for word_i in range(1, n):
             None
 
 
@@ -32,7 +42,7 @@ def get_args():
     Parse flags
     """
     parse = argparse.ArgumentParser()
-    parse.add_argument('--grammar', type=str, default='./sampleGrammer.cnf', help='training file path')
+    parse.add_argument('--grammar', type=str, default='./sampleGrammar.cnf', help='training file path')
 
     return parse.parse_args()
 
@@ -41,7 +51,7 @@ if __name__ == '__main__':
     logging.basicConfig(filename='./parser.log', level=logging.DEBUG)
     args = get_args()
     logging.info('Started')
-    gl = GrammarLoader(args.grammar)
+    gl = Parser(args.grammar)
     logging.info('Grammar loaded')
     while True:
         sent = input('Enter a sentence: ')
