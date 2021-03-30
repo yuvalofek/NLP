843ddf8f7948fa378259ed96c2a5c36499deca47
Yuval Ofek <35583988+yuvalofek@users.noreply.github.com>
2021-03-30 18:54:21 -0400

    initial functionality

--- Parser/main.py
+++ Parser/main.py
@@ -1,4 +1,26 @@
+import argparse
+
+
+class Parser:
+    def __init__(self, grammar):
+        self.grammar = grammar
+
+
+def get_args():
+    """
+    Parse flags
+    """
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--grammar_path', type=str, default=None, help='training file path')
+
+    return parse.parse_args()
 
 
 if __name__ == '__main__':
-    None
+    args = get_args()
+
+    while True:
+        sentence = input('Enter a sentence: ')
+
+        if sentence == 'quit':
+            break
