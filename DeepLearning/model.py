d19b754ec341648386488692610d0b566d681e9a
yuval <yuval.ofek007@gmail.com>
2021-05-05 17:08:01 -0400

    fix model and training

--- DeepLearning/model.py
+++ DeepLearning/model.py
@@ -1,17 +1,31 @@
 from torch.nn import Module
 import torch.nn as nn
-from torch.nn.functional import sigmoid
+from torch import sigmoid
 
 
 class SentimentModel(Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, batch_size):
         super(SentimentModel, self).__init__()
+        self.n_layers = 2
+        self.hidden_dim = hidden_dim
         self.word_embedding = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
-        self.output = nn.Linear(hidden_dim, 1)
+        self.l1 = nn.Linear(hidden_dim, 1)
+        self.sigmoid = sigmoid
 
-    def forward(self, sentence):
-        embeds = self.word_embedding(sentence)
-        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
-        prediction = sigmoid(self.output(lstm_out.view(len(sentence), -1)))
+    def forward(self, x):
+        batch_size = x.size()
+        # embedding
+        embeds = self.word_embedding(x)
+
+        # lstm
+        lstm_out, _ = self.lstm(embeds)
+        # lstm_out, _ = lstm_out.view(-1, self.hidden_dim)
+
+        # linear to sigmoid output
+        out = self.l1(lstm_out)
+        prediction = self.sigmoid(out)
+        prediction = prediction.view(batch_size, -1)
+        prediction = prediction[:, -1]
+
         return prediction
--- DeepLearning/train.py
+++ DeepLearning/train.py
@@ -1,35 +1,38 @@
 import argparse
+import numpy as np
+import torch
+from torch.optim import Adam
 from torch.utils.data import DataLoader
-from torch.autograd import Variable
 
 from data import SentimentDataset
 from prepro import Preprocessor
-from train import SentimentModel
-
-def binary_crossentropy(true, pred):
-    return true*torch.log(pred)
+from model import SentimentModel
 
 
-def train(tr, model, optimizer, loss):
+def binary_cross_entropy(true, prediction):
+    return true*torch.log(prediction)+prediction*torch.log(true)
+
+
+def train(training, model, optimizer, loss):
     epoch_loss = 0.0
-    for batch_i, batch in enumerate(training_set):
+    for idx, (tweet, label) in enumerate(training):
         # get the true values & make into cuda if needed
-        true = [torch.tensor(enc) for enc in batch]
 
+        # tweet = torch.tensor(tweet)
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward pass
-        pred = model(true)
+        prediction = model(tweet)
         # calculate multi-loss
-        lss = loss(true, pred)
+        lss = loss(prediction.squeeze(), label.float())
         # backward + optimize
         lss.backward()
         optimizer.step()
 
         # running sum
         epoch_loss += lss.item()
-        postfix = f'Training loss: {round(epoch_loss / (batch_i + 1), 4)}'
+        postfix = f'Training loss: {round(epoch_loss / (idx + 1), 4)}'
 
 
 def get_args():
@@ -41,6 +44,8 @@ def get_args():
     parser.add_argument('--validation_path', type=str, default=None, help='validation file path')
     parser.add_argument('--batch_size', type=int, default=16, help='training set batch size')
     parser.add_argument('--max_vocab', type=int, default=10_000, help='maximum vocab size')
+    parser.add_argument('--embedding_dim', type=int, default=6, help='size of embedding dim')
+    parser.add_argument('--hidden_dim', type=int, default=6, help='size of hidden layer')
     parser.add_argument('--save_path', type=str, default='./output.labels', help='file path for saved_model')
     return parser.parse_args()
 
@@ -50,15 +55,25 @@ if __name__ == '__main__':
     args = get_args()
 
     # make a dataset
-    training_set = SentimentDataset(dataset_path=args.train_path)
+    data = SentimentDataset(data=args.train_path)
 
     # preprocess and save word encodings
     preprocessor = Preprocessor(max_vocab=args.max_vocab)
-    training_set = preprocessor.fit_transform(dataset=training_set)
+    data = preprocessor.fit_transform(dataset=data)
     preprocessor.save()
 
-    # training_set = DataLoader(training_set, batch_size=args.batch_size, shuffle=True)
+    # validation split
+    data.split_data(validation_count=10_000)
+    train_ds, val_ds = data.to_dataset()
 
+    # to dataLoaders
+    train_set = DataLoader(train_ds, batch_size=args.batch_size, shuffle=True)
+    val_set = DataLoader(val_ds, batch_size=args.batch_size, shuffle=False)
+
+    mod = SentimentModel(len(preprocessor.vocab2enc)+3, args.embedding_dim, args.hidden_dim, args.batch_size)
+    opt = Adam(mod.parameters(), lr=0.01)
+
+    train(train_set, mod, opt, torch.nn.BCELoss())
 
 
 
