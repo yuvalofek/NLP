950e519b1a063d5606b14c5de4b335c44b85e8ab
yuval <yuval.ofek007@gmail.com>
2021-05-05 17:08:47 -0400

    splitting and setting up datasets

--- DeepLearning/data.py
+++ DeepLearning/data.py
@@ -1,4 +1,6 @@
-from torch.utils.data import Dataset
+import torch
+from torch.utils.data import TensorDataset
+import numpy as np
 import pandas as pd
 import logging
 
@@ -10,6 +12,7 @@ class SentimentDataset:
         else:
             self.data = data
         logging.info('Dataset Created')
+        self.validation = None
 
     def getitem(self, index):
         return self.data[index]
@@ -17,6 +20,22 @@ class SentimentDataset:
     def __len__(self):
         return len(self.data)
 
-    def get_data(self, validation_count):
+    def split_data(self, validation_count):
         # training set, validation set
-        return self.data[validation_count:], self.data[:validation_count]
+        self.validation = self.data[:validation_count]
+        self.data = self.data[validation_count:]
+        return self.data, self.validation
+
+    def to_dataset(self):
+        # to separate arrays
+        train_x, train_y = [item[1] for item in self.data], [item[0] for item in self.data]
+        train_x = np.array(train_x)
+        tr_ds = TensorDataset(torch.tensor(train_x, dtype=torch.long), torch.FloatTensor(train_y))
+
+        val_ds = None
+        if self.validation is not None:
+            val_x, val_y = [item[1] for item in self.validation], [item[0] for item in self.validation]
+            val_x = np.array(val_x)
+            val_ds = TensorDataset(torch.FloatTensor(val_x), torch.FloatTensor(val_y))
+
+        return tr_ds, val_ds
