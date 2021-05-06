c533375f42fbc470aa5559e8eaaedc976bd9078e
yuval <yuval.ofek007@gmail.com>
2021-05-05 21:17:36 -0400

    remove unused attributes

--- DeepLearning/model.py
+++ DeepLearning/model.py
@@ -6,12 +6,10 @@ from torch import sigmoid
 class SentimentModel(Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, batch_size):
         super(SentimentModel, self).__init__()
-        self.n_layers = 2
-        self.hidden_dim = hidden_dim
+
         self.word_embedding = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
         self.l1 = nn.Linear(hidden_dim, 1)
-        self.sigmoid = sigmoid
 
     def forward(self, x):
         batch_size = x.size()
@@ -22,9 +20,9 @@ class SentimentModel(Module):
         lstm_out, _ = self.lstm(embeds)
         # lstm_out, _ = lstm_out.view(-1, self.hidden_dim)
 
-        # linear to sigmoid output
+        # fully connected and sigmoid output
         out = self.l1(lstm_out)
-        prediction = self.sigmoid(out)
+        prediction = sigmoid(out)
         prediction = prediction.view(batch_size, -1)
         prediction = prediction[:, -1]
 
